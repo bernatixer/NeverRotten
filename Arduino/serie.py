9c5280a927ee3bdbf984afcbf5ffd067c1bfab33
dreaqdp <drea@Drea.localdomain>
2018-04-08 12:41:59 +0200

    py protocol comunication

--- Arduino/serie.py
+++ Arduino/serie.py
@@ -1,7 +1,11 @@
-import serial
+import serial, time
 
 arduino = serial.Serial('COM3', 9600, timeout = .1)
+time.sleep(1)
 while True: 
 	data = arduino.readline()[:-2]
-	if data:
-		print (data)
+	if data == "photo_prod" or data == "rept_photo":
+		#send a "take a photo" order
+		# send a string with the name of the product
+		arduino.write ();
+
